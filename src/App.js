22a72e236033c08906493aff0ecaaa439f9fe9ab
Ratan Hussain <mdratanmia7395@gmail.com>
2024-06-14 13:02:06 +0300

    initial commit
    I made this password generator for learning purpose only

--- src/App.js
+++ src/App.js
@@ -1,25 +1,99 @@
-import logo from './logo.svg';
+/** @format */
+
+import { useState } from 'react';
 import './App.css';
+import 'react-toastify/dist/ReactToastify.css';
+import { toast, ToastContainer } from 'react-toastify';
+import { capital, numbers, samplee, small } from './data';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	let [num, setNum] = useState(false);
+	let [lower, setLower] = useState(false);
+	let [upper, setUpper] = useState(false);
+	let [sample, setSample] = useState(false);
+	let [passLenght, setPassLenght] = useState(10);
+	let [Password, setPassword] = useState('');
+
+	let generate = () => {
+		let charAt = '';
+		let finalPass = '';
+		if (num || lower || upper || sample) {
+			if (num) charAt += numbers;
+			if (lower) charAt += small;
+			if (upper) charAt += capital;
+			if (sample) charAt += samplee;
+
+			for (let i = 0; i <= passLenght; i++) {
+				finalPass += charAt.charAt(Math.floor(Math.random() * charAt.length));
+			}
+			setPassword(finalPass);
+			toast.success('Passowrd generate success.');
+		} else {
+			toast.error('Please choose any of includes..');
+		}
+	};
+	let copyPass = () => {
+		navigator.clipboard.writeText(Password);
+		toast.success('Password Copyid');
+	};
+
+	return (
+		<div className='container-fluid mt-5'>
+			<ToastContainer />
+			<div className='container mt-5 pt-5'>
+				<div className='card p-2 bck'>
+					<h4 className='fw-bold text-center py-4'>Generate Random Password</h4>
+					<div className='d-flex'>
+						<input value={Password} readOnly className='form-control' />
+						<button className='btn btn-info fw-bold' onClick={copyPass}>
+							Copy
+						</button>
+					</div>
+					<div className='between p-2'>
+						<lavel className='fw-bold'>Password Lenght</lavel>
+						<input
+							type='number'
+							max={20}
+							min={10}
+							onChange={(e) => setPassLenght(e.target.value)}
+							value={passLenght}
+						/>
+					</div>
+					<div className='between p-2'>
+						<lavel className='fw-bold'>Include Numbers</lavel>
+						<input value={num} onChange={(e) => setNum(!num)} type='checkbox' />
+					</div>
+					<div className='between p-2'>
+						<lavel className='fw-bold'>Include lower letters</lavel>
+						<input
+							value={lower}
+							onChange={(e) => setLower(!lower)}
+							type='checkbox'
+						/>
+					</div>
+					<div className='between p-2'>
+						<lavel className='fw-bold'>Include upper letters</lavel>
+						<input
+							value={upper}
+							onChange={(e) => setUpper(!upper)}
+							type='checkbox'
+						/>
+					</div>
+					<div className='between p-2'>
+						<lavel className='fw-bold'>Include Symbols</lavel>
+						<input
+							value={sample}
+							onChange={(e) => setSample(!sample)}
+							type='checkbox'
+						/>
+					</div>
+					<button onClick={generate} className='btn fw-bold btn-info my-4'>
+						Generate Password
+					</button>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default App;
---
+++ src/data.jsx
@@ -0,0 +1,4 @@
+export let numbers = 123456789;
+export let capital = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+export let small = 'abcdefghijklmnopqrstuvwxyz';
+export let samplee = '!@#$%&*()';
